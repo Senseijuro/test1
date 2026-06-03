9d861582dd11ec9864ceaf5d2755d3b42f39d177
Senseijuro <yacinemeh@yahho.com>
2026-06-03 17:43:42 +0200

    TP1 - tests JUnit de la classe Voiture

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -2,7 +2,6 @@ package com.example.demo.data;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,7 +10,20 @@ public class VoitureTest {
 
     @Test
     void creerVoiture(){
-        assertEquals(1,1);
+        Voiture voiture = new Voiture("Renault", 10000);
+        assertEquals("Renault", voiture.getMarque());
+        assertEquals(10000, voiture.getPrix());
+    }
+
+    @Test
+    void modifierVoiture(){
+        Voiture voiture = new Voiture();
+        voiture.setMarque("Peugeot");
+        voiture.setPrix(20000);
+        voiture.setId(1);
+        assertEquals("Peugeot", voiture.getMarque());
+        assertEquals(20000, voiture.getPrix());
+        assertEquals(1, voiture.getId());
     }
 
 }
